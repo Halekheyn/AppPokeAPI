a5c2281322c19b855a020510be74771244cf12bc
Mauricio Patiño <mapal0925@gmail.com>
2024-02-23 19:22:06 -0500

    Se cofigura el consumo de la API de Pokémon, para obtener al lista y paginación

--- src/app/table/crud/crud.component.ts
+++ src/app/table/crud/crud.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonData } from '../interfaces/pokemon-table.interface';
+import { CrudService } from './crud.service';
+
 
 @Component({
   selector: 'table-crud',
   templateUrl: './crud.component.html',
   styleUrls: ['./crud.component.scss']
 })
-export class CrudComponent {
+export class CrudComponent implements OnInit {
 
+  public pokemonData!: PokemonData[];
+
+
+  constructor(private _crudService:CrudService ){}
+
+  ngOnInit(): void {
+
+    this.pokemonList(20,20);
+  }
+
+  public pokemonList( limit:number, offset:number ){
+    this._crudService.getPokemonList(limit, offset)
+        .subscribe( pokemons => {
+
+          this.pokemonData = pokemons?.results ?? [];
+        })
+  }
 }
---
+++ src/app/table/crud/crud.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of } from "rxjs";
+
+import { PokemonTable } from "../interfaces/pokemon-table.interface";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService{
+
+  private url_api: string = "https://pokeapi.co/api/v2/ability"
+
+  constructor(private httpClient: HttpClient){}
+
+  getPokemonList( limit:number, offset:number): Observable<PokemonTable | null>{
+
+    const url: string = `${ this.url_api }/?limit=${ limit }&offset=${ offset }`;
+
+    return this.getPokemonRequest(url);
+  }
+
+  private getPokemonRequest( url:string ):Observable<PokemonTable | null> {
+    console.log('url', url);
+
+    return this.httpClient.get<PokemonTable>(url)
+               .pipe(
+                  catchError(error => {
+                    console.error('getPokemonRequest - catchError', error);
+                    return of(null);
+                  })
+               )
+  }
+}
